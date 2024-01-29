419d9fdb83f7e76cc75521f10ae17024726bbcfb
Swapnil <swapnilnegi377@gmail.com>
2024-01-29 10:46:09 +0530

    update private component

--- src/components/auth components/Private.tsx
+++ src/components/auth components/Private.tsx
@@ -1,10 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const Private = () => {
+  const [data, setData] = useState([]);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      setIsConnected(true);
+    } else {
+      getData();
+    }
+  }, [data]);
+
+  const getData = async () => {
+    await axios
+      .get("https://gamestore-api-3gln.onrender.com/get-new-releases")
+      .then((res) => {
+        setData(res.data);
+      });
+  };
   let authUser = localStorage.getItem("user");
   let authGuest = localStorage.getItem("guest");
 
-  return authUser || authGuest ? <Outlet /> : <Navigate to={"/login"} />;
+  return !isConnected ? (
+    <div>Connecting to the server...</div>
+  ) : authUser || authGuest ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/login"} />
+  );
 };
 
 export default Private;
